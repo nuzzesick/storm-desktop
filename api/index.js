bc0cef666c226b47f753c757bd1f254634da94a4
nuzzesick <mati280341@gmail.com>
2021-05-17 18:27:16 -0300

    calling api when starting electron app

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const WebTorrent = require('webtorrent-hybrid');
 
-const app = express();
+const server = express();
 const port = 3000;
 
 const client = new WebTorrent();
 
-app.get('/download', (req, res, next) => {
+server.get('/download', (req, res, next) => {
   const startTorrent = () => new Promise((resolve) => {
     const { id: torrentId } = req.query;
     client.add(torrentId, { path: 'downloads' }, (torrent) => {
@@ -20,12 +20,12 @@ app.get('/download', (req, res, next) => {
   startTorrent().then(() => { next(); });
 });
 
-app.get('/info', (req, res, next) => {
+server.get('/info', (req, res, next) => {
   const getTorrentInfo = () => new Promise((resolve) => {
     const { id: torrentId } = req.query;
     const torrent = client.get(torrentId);
     const {
-      created, createdBy, numPeers, progress, path, ready, uploadSpeed, downloadSpeed,
+      created, createdBy, numPeers, progress, path: pathname, ready, uploadSpeed, downloadSpeed,
       timeRemaining, done,
     } = torrent;
     res.json({
@@ -33,7 +33,7 @@ app.get('/info', (req, res, next) => {
       createdBy,
       numPeers,
       progress,
-      path,
+      pathname,
       ready,
       uploadSpeed,
       downloadSpeed,
@@ -45,6 +45,6 @@ app.get('/info', (req, res, next) => {
   getTorrentInfo().then(() => { next(); });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Storm server running at http://localhost:${port}`);
 });
--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,8 +1,11 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
+const server = require('../api/index');
 
-function createWindow() {
+server();
+
+const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -17,30 +20,17 @@ function createWindow() {
     protocol: 'file:',
     slashes: true,
   });
-
-  // and load the index.html of the app.
   mainWindow.loadURL(startUrl);
+};
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
 
   app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
