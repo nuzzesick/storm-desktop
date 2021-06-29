374d5bcea1d835e6ca2fc2245d578525bfc54390
morbidgus <madridgustavo23@gmail.com>
2021-06-29 16:45:15 -0300

    adding again the toolbar

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,3 @@
 export { default as Navbar } from './Navbar';
-export { default as TopBar } from './TopBar';
+export { default as Toolbar } from './toolbar/Toolbar.component';
 export { default as Table } from './Table';
