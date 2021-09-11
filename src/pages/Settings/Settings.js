6802356c09f11587fe2e30404abbe4035e64bfe6
nuzzesick <mati280341@gmail.com>
2021-09-11 19:27:06 -0300

    Added about us section

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import StormContext from '../../context/Storm.context';
 import {
   Container,
   TopContainer,
-  BackButton,
+  BackButtonContainer,
+  ActionButton,
   BackIcon,
   DropdownContainer,
   DropdownButton,
@@ -14,20 +15,37 @@ import {
   SettingContainer,
   Directory,
   DirectoryButton,
+  AboutUsModal,
+  AboutUsModalContent,
+  AboutUsTopContent,
+  AboutUsTitle,
+  CloseIcon,
+  AboutUsContent,
+  ContributeButton,
+  StormLogo,
+  GitHubIcon,
 } from './Settings.styles';
 
 const Settings = () => {
   const stormContext = useContext(StormContext);
   const { data: { downloadsDirectory }, actions: { changeDownloadsDirectory } } = stormContext;
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleModal = () => {
+    setModalOpen(!modalOpen);
+  };
+
   return (
     <Container>
       <TopContainer>
-        <Link to="/">
-          <BackButton>
-            <BackIcon />
-            Back
-          </BackButton>
-        </Link>
+        <BackButtonContainer>
+          <ActionButton>
+            <Link to="/">
+              <BackIcon />
+              Back
+            </Link>
+          </ActionButton>
+        </BackButtonContainer>
         <DropdownContainer>
           Settings:
           <DropdownButton type="button">
@@ -35,7 +53,9 @@ const Settings = () => {
             {/* <DownArrowIcon /> */}
           </DropdownButton>
         </DropdownContainer>
-        <AboutContainer />
+        <AboutContainer>
+          <ActionButton onClick={handleModal}>About us</ActionButton>
+        </AboutContainer>
       </TopContainer>
       <Content>
         <SettingContainer>
@@ -44,6 +64,46 @@ const Settings = () => {
           <DirectoryButton type="button" onClick={changeDownloadsDirectory}>{downloadsDirectory ? 'Change directory' : 'Set directory'}</DirectoryButton>
         </SettingContainer>
       </Content>
+      {
+        modalOpen && (
+          <AboutUsModal>
+            <AboutUsModalContent>
+              <AboutUsTopContent>
+                <AboutUsTitle>
+                  <StormLogo />
+                  About us
+                </AboutUsTitle>
+                <ActionButton onClick={handleModal}>
+                  <CloseIcon />
+                </ActionButton>
+              </AboutUsTopContent>
+              <AboutUsContent>
+                <span>
+                  Storm comes with the idea of
+                  {' '}
+                  <b>change the way we see torrent clients</b>
+                  , because all of them always have those typical tables with a
+                  lot of data that most of the regular users doesn&apos;t understand
+                  it all, including us.
+                  <br />
+                  We tried to put just the necessary features with a beautiful UI/UX.
+                  <br />
+                  <br />
+                  Storm is an open source project, so if you want to help us to build the
+                  best torrent client ever, this is the best time for you.
+                  <br />
+                  <br />
+                  <br />
+                </span>
+                <ContributeButton href="https://github.com/nuzzesick/storm-desktop" target="_blank" rel="noreferrer">
+                  <GitHubIcon />
+                  Start contributing
+                </ContributeButton>
+              </AboutUsContent>
+            </AboutUsModalContent>
+          </AboutUsModal>
+        )
+      }
     </Container>
   );
 };
--- src/pages/Settings/Settings.styles.js
+++ src/pages/Settings/Settings.styles.js
@@ -16,13 +16,18 @@ export const TopContainer = styled.div`
   justify-content: space-between;
   width: 100%;
   a {
-    width: 16rem;
+    text-decoration: none;
   }
 `;
 
-export const BackButton = styled.span`
+export const BackButtonContainer = styled.div`
+  width: calc(100% / 3);
+`;
+
+export const ActionButton = styled.span`
   display: flex;
   align-items: center;
+  color: ${Colors.NEUTRAL_WHITE};
   margin: -0.5rem -1rem;
   padding: 0.5rem 1rem;
   background: transparent;
@@ -30,10 +35,12 @@ export const BackButton = styled.span`
   border-radius: 4px;
   transition: 0.3s;
   font-size: 1.1rem;
-  color: ${Colors.NEUTRAL_WHITE};
   width: fit-content;
   &:hover {
     background: ${Colors.HIGHLIGHT2};
+  }
+  a {
+    color: ${Colors.NEUTRAL_WHITE};
   }
 `;
 
@@ -48,7 +55,7 @@ export const DropdownContainer = styled.div`
   align-items: center;
   font-size: 1.1rem;
   color: ${Colors.HIGHLIGHT4};
-  width: 16rem;
+  width: calc(100% / 3);
   justify-content: center;
 `;
 
@@ -68,11 +75,9 @@ export const DownArrowIcon = styled(Vectors.downArrowIcon)`
 `;
 
 export const AboutContainer = styled.div`
-  width: 16rem;
+  width: calc(100% / 3);
   display: flex;
   justify-content: flex-end;
-  font-size: 1.1rem;
-  color: ${Colors.NEUTRAL_WHITE};
 `;
 
 export const Content = styled.div`
@@ -112,4 +117,83 @@ export const DirectoryButton = styled.div`
   background-color: #232323;
   cursor: pointer;
   margin-left: 1rem;
+`;
+
+export const AboutUsModal = styled.div`
+  position: fixed;
+  background-color: #000000c7;
+  height: 100%;
+  margin: auto;
+  top: 0;
+  right: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const AboutUsModalContent = styled.div`
+  width: calc(100% - 50rem);
+  padding: 4rem 8rem;
+  background: ${Colors.BASE_DARK1};
+  border-radius: 8px;
+  box-shadow: 0px 0px 8px 0px #1010108a;
+  color: ${Colors.NEUTRAL_WHITE};
+  margin: 8% auto;
+`;
+
+export const AboutUsTopContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 3rem;
+`;
+
+export const AboutUsTitle = styled.div`
+  font-size: 1.4rem;
+  font-weight: 600;
+  color: ${Colors.NEUTRAL_WHITE};
+  display: flex;
+  align-items: center;
+  margin: 0;
+`;
+
+export const CloseIcon = styled(Vectors.closeIcon)`
+  width: 1rem;
+  fill: ${Colors.NEUTRAL_WHITE};
+`;
+
+export const AboutUsContent = styled.div`
+  margin-top: 1.6rem;
+  display: flex;
+  font-size: 1.12rem;
+  flex-direction: column;
+  color: ${Colors.BASE_DARK4};
+  line-height: 1.8rem;
+`;
+
+export const ContributeButton = styled.a`
+  display: flex;
+  align-items: center;
+  background-color: ${Colors.HIGHLIGHT2};
+  color: ${Colors.NEUTRAL_WHITE};
+  width: fit-content;
+  padding: 0.2rem 1.2rem;
+  font-size: 1.05rem;
+  font-weight: 500;
+  border-radius: 3px;
+  transition: 0.3s;
+  &:hover {
+    background-color: ${Colors.HIGHLIGHT1};
+  }
+`;
+
+export const StormLogo = styled(Vectors.logo)`
+  width: 1.6rem;
+  margin-right: 0.4rem;
+`;
+
+export const GitHubIcon = styled(Vectors.githubIcon)`
+  width: 0.9rem;
+  fill: ${Colors.NEUTRAL_WHITE};
+  margin-right: 0.4rem;
 `;
