e94a34b16916876fa519604bb64aff2d1f00e605
Federico Galvan <Fgalvan@irsacorp.com.ar>
2021-09-22 19:15:57 -0300

    Home | Remove Fragment and data from context

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'import/prefer-default-export': 'off',
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    "linebreak-style": 0
   },
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,34 +1,21 @@
 /* eslint-disable react/jsx-fragments */
-import React, { Fragment, useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Navbar, Toolbar, TorrentsList } from '../../containers';
 import StormContext from '../../context/Storm.context';
 import { HomePageContainer } from './Home.styles';
 
 const Home = () => {
   const [activeFilter, setActiveFilter] = useState('all');
-  const {
-    data:
-    { torrentSelected },
-    actions: { clearTorrentSelection },
-  } = useContext(StormContext);
+  const { actions: { clearTorrentSelection } } = useContext(StormContext);
+
   return (
-    <Fragment>
-      <Navbar
-        activeFilter={activeFilter}
-        setActiveFilter={setActiveFilter}
-      />
+    <>
+      <Navbar activeFilter={activeFilter} setActiveFilter={setActiveFilter} />
       <Toolbar setActiveFilter={setActiveFilter} />
-      <HomePageContainer onClick={() => {
-        if (torrentSelected) {
-          clearTorrentSelection();
-        }
-      }}
-      >
-        <TorrentsList
-          activeFilter={activeFilter}
-        />
+      <HomePageContainer onClick={() => clearTorrentSelection()}>
+        <TorrentsList activeFilter={activeFilter} />
       </HomePageContainer>
-    </Fragment>
+    </>
   );
 };
 
