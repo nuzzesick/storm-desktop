309f1a05660bffbb980b9480930c085e92fd9efd
Federico Galvan <Fgalvan@irsacorp.com.ar>
2021-09-22 20:05:42 -0300

    Torrents List to a Handler + Conditional rendering

--- src/containers/TorrentsList/TorrentsList.js
+++ src/containers/TorrentsList/TorrentsList.js
@@ -1,71 +1,60 @@
 import PropTypes from 'prop-types';
 import React, { useContext, useState, useEffect } from 'react';
 import StormContext from '../../context/Storm.context';
-import {
-  AppContent, TorrentsGrid, Content, EmptyState, EmptyTitle, EmptySubtitle, LoadingContent,
-} from './TorrentsList.styles';
+import { AppContent, TorrentsGrid, Content, EmptyState, EmptyTitle, EmptySubtitle, LoadingContent} from './TorrentsList.styles';
 import Loading from '../../components/Loading/Loading';
 import EmptyStateIcon from './EmptyStateIcon';
 import TorrentCard from '../../components/TorrentCard/TorrentCard';
+import when from '../../helpers/when';
+import torrentListHandler from '../../helpers/torrentListHandler'
 
 const TorrentsList = ({ activeFilter }) => {
-  const {
-    data:
-    { torrentsList },
-  } = useContext(StormContext);
+  const { data: { torrentsList }} = useContext(StormContext);
   const isLoading = torrentsList === null;
   const userHasTorrents = torrentsList && torrentsList.length > 0;
   const [torrentsToShow, setTorrentsToShow] = useState(torrentsList);
+
   useEffect(() => {
-    if (torrentsList) {
-      if (activeFilter === 'all') {
-        setTorrentsToShow(torrentsList);
-      } else if (activeFilter === 'downloading') {
-        setTorrentsToShow(torrentsList.filter((torrent) => !torrent.paused));
-      } else if (activeFilter === 'seeding') {
-        setTorrentsToShow(torrentsList.filter((torrent) => !torrent.paused && torrent.done));
-      } else if (activeFilter === 'paused') {
-        setTorrentsToShow(torrentsList.filter((torrent) => torrent.paused));
-      } else if (activeFilter === 'completed') {
-        setTorrentsToShow(torrentsList.filter((torrent) => torrent.done));
-      }
-    }
+      setTorrentsToShow(torrentListHandler(activeFilter, torrentsList))
   }, [torrentsList, activeFilter]);
-  if (isLoading) {
-    return (
-      <Content>
-        <LoadingContent>
-          <Loading />
-        </LoadingContent>
-      </Content>
-    );
-  }
-  if (!userHasTorrents) {
-    return (
-      <Content>
-        <EmptyState>
-          <EmptyStateIcon />
-          <EmptyTitle>No torrents added yet</EmptyTitle>
-          <EmptySubtitle>Start adding torrents and join the amazing world of P2P!</EmptySubtitle>
-        </EmptyState>
-      </Content>
-    );
-  }
+
   return (
-    <AppContent>
-      {
-        torrentsToShow && torrentsToShow.length > 0
-          && (
-          <TorrentsGrid>
-            {
-              torrentsToShow.map((torrent) => (
-                <TorrentCard torrent={torrent} key={`torrent-${torrent.magnetURI}`} />
-              ))
-            }
-          </TorrentsGrid>
-          )
-      }
-    </AppContent>
+    <>
+      {when(isLoading)(
+        <Content>
+          <LoadingContent>
+            <Loading />
+          </LoadingContent>
+        </Content>
+      )}
+
+      {when(!userHasTorrents && !isLoading)(
+        <Content>
+          <EmptyState>
+            <EmptyStateIcon />
+            <EmptyTitle>No torrents added yet</EmptyTitle>
+            <EmptySubtitle>
+              Start adding torrents and join the amazing world of P2P!
+            </EmptySubtitle>
+          </EmptyState>
+        </Content>
+      )}
+
+      {when(!isLoading)(
+        <AppContent>
+          {torrentsToShow && torrentsToShow.length > 0 && (
+            <TorrentsGrid>
+              {torrentsToShow.map((torrent) => (
+                <TorrentCard
+                  torrent={torrent}
+                  key={`torrent-${torrent.magnetURI}`}
+                />
+              ))}
+            </TorrentsGrid>
+          )}
+        </AppContent>
+      )}
+    </>
   );
 };
 
